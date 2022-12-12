be762e78cfbcaa0742dd0da1a7472440659ee59f
austinjjohn <ajohn8@umbc.edu>
2022-12-11 22:21:10 -0500

    Pings user. Fixed Intents

--- Discord_Bot/main.py
+++ Discord_Bot/main.py
@@ -1,19 +1,24 @@
 import preprocess  # Stores all pre-processing code for user strings
 import discord
 import os
+from dotenv import load_dotenv
 from transformers import AutoModel, AutoTokenizer
 import keras
 import numpy as np
 
 # Creating client instance - this will be used to interact with the Discord API (connection to Discord)
-# intents = discord.Intents.all()
 
+
+load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
-# Saw the command below in the Discord API Man Pages. Manpages said to use default intents.
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-# secret_key = ''
 
+# Saw the command below in the Discord API Man Pages. Manpages said to use default intents.
+# intents = discord.Intents.default()
+# intents = discord.Intents(messages=True)
+# intents.messages = True
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 # my_guild = os.getenv("DISCORD_GUILD")
 
 
@@ -29,6 +34,9 @@ async def on_ready():
     # Verify that the bot has successfully come online
     print(f"Bot logged in as {client.user}")
 
+@client.event
+async def on_member_join(member):
+    await member.channel.send(f"Welcome, There are no rules because I am the allseer and will squash your hopes and dreams of being toxic.")
 
 # The on_message event happens when a message gets sent on the server
 @client.event
@@ -54,10 +62,14 @@ async def on_message(message):
         model_prediction = moderation_model.predict(model_list)[0][0]
 
         # print(model_prediction)
-        print(model_prediction)
+
         # Code to send messages. We'll be using this function to have our bot send a message flagging somehting as toxic.
         await message.channel.send(model_prediction)
-
+        
+        if model_prediction > 50:
+            await client.get_channel(1051683399720505385).send(f'Requires Moderation: {message.content}')
+            # await message.channel.send(f'{message.id} {message.content} moderation')
+            await message.channel.send(f'{message.author.mention} Abstain from sending toxic messages in the chat.')
 
 # client.run(secret_key)  # Running the bot
 client.run(token)
